da0a23d6ed5ad5c5f1fb5c38c4c417e4f6af5d16
Alex Gusev <alex@flancer64.com>
2015-11-06 16:07:05 +0200

    MOBI-107 - Create operation service in Accounting module

--- test/functional/Setup/Schema_ManualTest.php
+++ test/functional/Setup/Schema_ManualTest.php
@@ -12,7 +12,7 @@ include_once(__DIR__ . '/../phpunit_bootstrap.php');
 class Schema_ManualTest extends \PHPUnit_Framework_TestCase {
 
     function test_install() {
-        $obm = Context::get()->getObjectManager();
+        $obm = Context::instance()->getObjectManager();
         $setup = $obm->get('\Magento\Setup\Module\Setup');
         $context = $obm->create('\Magento\Setup\Model\ModuleContext', [ 'version' => '' ]);
         $obj = $obm->get('\Praxigento\Accounting\Setup\Schema');
--- test/integration/Simple_Test.php
+++ test/integration/Simple_Test.php
@@ -5,13 +5,15 @@
 
 namespace Praxigento\Accounting;
 
+use Praxigento\Core\Lib\Context;
+
 include_once(__DIR__ . '/phpunit_bootstrap.php');
 
 class Simple_Test extends \PHPUnit_Framework_TestCase {
 
 
     public function  test() {
-        $lib = new \Praxigento\Accounting\Test\Integration\Simple();
+        $lib = Context::instance()->getObjectManager()->get('\Praxigento\Accounting\Test\Integration\Simple');
         $lib->dataClean();
         $lib->dataInit();
         /* Perform assets validation */
