167289947ed5ceca687565d05fd79407e509717f
Alex Gusev <alex@flancer64.com>
2016-04-06 17:48:16 +0300

    MOBI-265 - clean up old Service related code.

--- src/Lib/Service/Type/Operation/Request/GetByCode.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * User: Alex Gusev <alex@flancer64.com>
- */
-namespace Praxigento\Accounting\Lib\Service\Type\Operation\Request;
-
-class GetByCode extends \Praxigento\Core\Lib\Service\Type\Base\Request\GetByCode {
-}
