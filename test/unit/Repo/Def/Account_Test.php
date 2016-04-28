34720d4d210d0842ef28224eeec5b27cfe16e1ae
Alex Gusev <alex@flancer64.com>
2016-04-28 10:39:42 +0300

    MOBI-280 - Unit tests and code coverage after grids (account repo unit tests)

--- test/unit/Repo/Def/Account_Test.php
+++ test/unit/Repo/Def/Account_Test.php
@@ -18,7 +18,7 @@ class Account_UnitTest extends \Praxigento\Core\Test\BaseMockeryCase
     private $mRepoGeneric;
     /** @var  \Mockery\MockInterface */
     private $mRsrcConn;
-    /** @var  \Praxigento\Accounting\Repo\Entity\Type\Def\Asset */
+    /** @var  Account */
     private $obj;
 
     public function setUp()
@@ -36,6 +36,57 @@ class Account_UnitTest extends \Praxigento\Core\Test\BaseMockeryCase
     public function test_constructor()
     {
         /* === Call and asserts  === */
+        $this->assertInstanceOf(IAccount::class, $this->obj);
+    }
+
+    public function test_getByCustomerId()
+    {
+        /* === Test Data === */
+        $CUST_ID = 32;
+        $ASSET_TYPE_ID = 45;
+        $DATA = [['some data' => 'here']];
+        /* === Setup Mocks === */
+        // $result = $this->get($where);
+        // $result = $this->_repoGeneric->getEntities($this->_entityName, null, $where, $order, $limit, $offset);
+        $this->mRepoGeneric
+            ->shouldReceive('getEntities')
+            ->andReturn($DATA);
+        /* === Call and asserts  === */
+        $res = $this->obj->getByCustomerId($CUST_ID, $ASSET_TYPE_ID);
+        $this->assertInstanceOf(IAccount::class, $this->obj);
+    }
+
+    public function test_updateBalance_negative()
+    {
+        /* === Test Data === */
+        $ACC_ID = 32;
+        $DELTA = -45;
+        $ROWS_UPDATED = 1;
+        /* === Setup Mocks === */
+        // $rowsUpdated = $this->updateById($accountId, $bind);
+        // $result = $this->_repoGeneric->updateEntity($this->_entityName, $data, $where);
+        $this->mRepoGeneric
+            ->shouldReceive('updateEntity')
+            ->andReturn($ROWS_UPDATED);
+        /* === Call and asserts  === */
+        $res = $this->obj->updateBalance($ACC_ID, $DELTA);
+        $this->assertInstanceOf(IAccount::class, $this->obj);
+    }
+
+    public function test_updateBalance_positive()
+    {
+        /* === Test Data === */
+        $ACC_ID = 32;
+        $DELTA = 45;
+        $ROWS_UPDATED = 1;
+        /* === Setup Mocks === */
+        // $rowsUpdated = $this->updateById($accountId, $bind);
+        // $result = $this->_repoGeneric->updateEntity($this->_entityName, $data, $where);
+        $this->mRepoGeneric
+            ->shouldReceive('updateEntity')
+            ->andReturn($ROWS_UPDATED);
+        /* === Call and asserts  === */
+        $res = $this->obj->updateBalance($ACC_ID, $DELTA);
         $this->assertInstanceOf(IAccount::class, $this->obj);
     }
 }
