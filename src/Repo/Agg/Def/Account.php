42bfcc738e4891daffa92b7e2222e8e54a621745
Alex Gusev <alex@flancer64.com>
2016-09-20 15:30:20 +0300

    MOBI-446 - Create aggregate to use in Accounts grid (mlm id column)

--- src/Repo/Agg/Def/Account.php
+++ src/Repo/Agg/Def/Account.php
@@ -12,32 +12,13 @@ class Account
     implements \Praxigento\Accounting\Repo\Agg\IAccount
 {
 
-    /** @var  \Magento\Framework\DB\Adapter\AdapterInterface */
-    protected $_conn;
     /** @var Account\SelectFactory */
     protected $_factorySelect;
-    /** @var  \Magento\Framework\ObjectManagerInterface */
-    protected $_manObj;
-    /** @var  \Praxigento\Core\Transaction\Database\IManager */
-    protected $_manTrans;
-    /** @var \Praxigento\Core\Repo\IGeneric */
-    protected $_repoGeneric;
-    /** @var \Magento\Framework\App\ResourceConnection */
-    protected $_resource;
+
 
     public function __construct(
-        \Magento\Framework\ObjectManagerInterface $manObj,
-        \Praxigento\Core\Transaction\Database\IManager $manTrans,
-        \Magento\Framework\App\ResourceConnection $resource,
-        \Praxigento\Core\Repo\IGeneric $repoGeneric,
-        \Praxigento\Warehouse\Repo\Entity\IWarehouse $repoEntityWarehouse,
         Account\SelectFactory $factorySelect
     ) {
-        $this->_manObj = $manObj;
-        $this->_manTrans = $manTrans;
-        $this->_resource = $resource;
-        $this->_conn = $resource->getConnection();
-        $this->_repoGeneric = $repoGeneric;
         $this->_factorySelect = $factorySelect;
     }
 
