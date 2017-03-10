331f840c13acea579444d969ca503bac99596a2b
Alex Gusev <alex@flancer64.com>
2017-03-10 20:14:29 +0200

    MOBI-633 - Add security for DCP operations

--- src/Api/Transaction/Get.php
+++ src/Api/Transaction/Get.php
@@ -8,15 +8,22 @@ namespace Praxigento\Accounting\Api\Transaction;
 class Get
     implements \Praxigento\Accounting\Api\Transaction\GetInterface
 {
+    /** @var \Praxigento\Core\Api\IAuthenticator */
+    protected $authenticator;
 
-    public function __construct()
+    public function __construct(
+        \Praxigento\Core\Api\IAuthenticator $authenticator
+    )
     {
-
+        $this->authenticator = $authenticator;
     }
 
     public function exec(\Praxigento\Accounting\Api\Transaction\Get\Request $data)
     {
         $result = new \Praxigento\Accounting\Api\Transaction\Get\Response();
+        $isAuthenticated = $this->authenticator->isAuthenticated();
+        $user = $this->authenticator->getCurrentUserData();
+        $arr = (array)$user->get();
         return $result;
     }
 
