1ff33868f28ddafb43e8a218e80a944d78175510
dmitriimakhov <dmitriimakhov@gmail.com>
2017-09-18 15:14:04 +0300

    MOBI-899 # Refactor other grids in Accounting module \Praxigento\Accounting\Ui\DataProvider\Type\Oper

---
+++ src/Ui/DataProvider/Grid/Type/Operation/QueryBuilder.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Praxigento\Accounting\Ui\DataProvider\Grid\Type\Operation;
+
+use Praxigento\Accounting\Repo\Entity\Data\Type\Operation as ETypeOperation;
+
+class QueryBuilder
+    extends \Praxigento\Core\Ui\DataProvider\Grid\Query\Builder
+{
+    /**#@+ Tables aliases for external usage ('camelCase' naming) */
+    const AS_TYP_OPERATION = 'top';
+    /**#@- */
+    const A_CODE = 'code';
+    /**#@+
+     * Aliases for data attributes.
+     */
+    const A_ID_ASSET = 'id';
+    const A_NOTE = 'note';
+
+    /**#@- */
+
+    protected function getMapper()
+    {
+        if (is_null($this->mapper)) {
+            $map = [
+                self::A_ID_ASSET => self::AS_TYP_OPERATION . '.' . ETypeOperation::ATTR_ID,
+                self::A_CODE => self::AS_TYP_OPERATION . '.' . ETypeOperation::ATTR_CODE,
+                self::A_NOTE => self::AS_TYP_OPERATION . '.' . ETypeOperation::ATTR_NOTE
+            ];
+            $this->mapper = new \Praxigento\Core\Repo\Query\Criteria\Def\Mapper($map);
+        }
+        $result = $this->mapper;
+        return $result;
+    }
+
+    protected function getQueryItems()
+    {
+        $result = $this->conn->select();
+        /* define tables aliases for internal usage (in this method) */
+        $asTypAsset = self::AS_TYP_OPERATION;
+
+        /* SELECT FROM prxgt_acc_type_asset */
+        $tbl = $this->resource->getTableName(ETypeOperation::ENTITY_NAME);
+        $as = $asTypAsset;
+        $cols = [
+            self::A_ID_ASSET => ETypeOperation::ATTR_ID,
+            self::A_CODE => ETypeOperation::ATTR_CODE,
+            self::A_NOTE => ETypeOperation::ATTR_NOTE
+        ];
+        $result->from([$as => $tbl], $cols);
+        return $result;
+    }
+
+    protected function getQueryTotal()
+    {
+        /* get query to select items */
+        /** @var \Magento\Framework\DB\Select $result */
+        $result = $this->getQueryItems();
+        /* ... then replace "columns" part with own expression */
+        $value = 'COUNT(' . self::AS_TYP_OPERATION . '.' . ETypeOperation::ATTR_ID . ')';
+
+        /**
+         * See method \Magento\Framework\DB\Select\ColumnsRenderer::render:
+         */
+        /**
+         * if ($column instanceof \Zend_Db_Expr) {...}
+         */
+        $exp = new \Praxigento\Core\Repo\Query\Expression($value);
+        /**
+         *  list($correlationName, $column, $alias) = $columnEntry;
+         */
+        $entry = [null, $exp, null];
+        $cols = [$entry];
+        $result->setPart('columns', $cols);
+        return $result;
+    }
+
+}
