b26b9c709678dd704ccd18b800abfe0d71163a49
Alex Gusev <alex@flancer64.com>
2017-11-21 12:38:41 +0200

    MOBI-992 Add UI component to perform asset transfers using admin

--- src/view/adminhtml/web/js/customer/accounting.js
+++ src/view/adminhtml/web/js/customer/accounting.js
@@ -1,23 +1,40 @@
 define([
     'jquery',
+    'ko',
     'uiComponent',
     'uiRegistry',
     'Magento_Ui/js/modal/modal',
     'mage/template',
-    'text!Praxigento_Accounting/templates/modal/accounting.html'
-], function ($, Component, uiRegistry, Modal, mageTemplate, innerHtml) {
+    'text!Praxigento_Accounting/templates/customer/accounting/slider.html'
+], function ($, ko, Component, uiRegistry, Modal, mageTemplate, innerHtml) {
 
     /**
-     * Collect working data into the local context.
+     * Collect working data into the local context (scope).
      */
-    /* save globals FROM_KEY */
+    /* component constants */
+    var TYPE_DIRECT = 'direct';
+    var TYPE_BETWEEN = 'between';
+    /* pin globals into UI component scope */
     var baseUrl = BASE_URL;
-    var originUrl = origin; // global var from hell (I don't know from where)
-    var formKey = FORM_KEY;
     /* get customer data from uiRegistry */
     var customerDs = uiRegistry.get('customer_form.customer_form_data_source');
     var customer = customerDs.data.customer;
     var customerId = customer.entity_id;
+    /* define local working data */
+    var baseAdminUrl = baseUrl.replace('/customer/', '/');
+    /* see \Praxigento\Downline\Controller\Adminhtml\Customer\Search */
+    var urlCustomerSearch = baseAdminUrl + 'prxgt_dwnl/customer/search/';
+    /* View Model for slider */
+    var viewModel = {
+        assets: undefined,
+        customer: undefined,
+        selectedCounterparty: undefined,
+        amount: ko.observable(0),
+        counterparty: ko.observable(),
+        selectedAsset: ko.observable(),
+        transferType: ko.observable(TYPE_DIRECT)
+    };
+
 
     /**
      * Front-back communication functions
@@ -28,15 +45,69 @@ define([
         $('body').trigger('processStart');
         var pinCustomerId = customerId; // pin external scope data
 
-        /* define function to process response from server */
-        var fnSuccess = function (data, status, xhr) {
-            if (data.error) {
-                alert('Error: ' + data.message);
-            } else {
-                alert(JSON.stringify(data));
+        /* process response from server: create modal slider and populate with data */
+        var fnSuccess = function (data) {
+            /**
+             * Definitions.
+             */
+            var options = {
+                type: 'slide',
+                responsive: true,
+                innerScroll: true,
+                clickableOverlay: true,
+                title: $.mage.__('Accounting'),
+                buttons: [{
+                    text: $.mage.__('Cancel'),
+                    class: '',
+                    click: function () {
+                        this.closeModal();
+                    }
+                }, {
+                    text: $.mage.__('Process'),
+                    class: '',
+                    click: fnAjaxProcessData
+                }]
+            };
+
+            /* populate template with initial data */
+            var fnModalOpen = function () {
+                /* set parsed HTML as content for modal placeholder create modal slider */
+                $('#modal_panel_placeholder').html(innerHtml);
+                var popup = Modal(options, $('#modal_panel_placeholder'));
+
+                /* open modal slider, populate knockout view-model and bind it to template */
+                popup.openModal();
+                viewModel.amount = ko.observable(0);
+                viewModel.assets = data.assets;
+                viewModel.counterparty = ko.observable();
+                viewModel.customer = data.customer;
+                viewModel.transferType = ko.observable(TYPE_DIRECT);
+                var elm = document.getElementById('modal_panel_placeholder');
+                ko.cleanNode(elm);
+                ko.applyBindings(viewModel, elm);
             }
+
+            /**
+             * Processing.
+             */
             /* switch off loader */
             $('body').trigger('processStop');
+            /* open slider */
+            fnModalOpen();
+
+            /* add JQuery auto complete widget to the slider */
+            $('#prxgtCustomerSearch').autocomplete({
+                source: fnAjaxCustomerSearch,
+                select: fnAutocompleteSelected,
+                minLength: 2,
+                /* disable auto-complete helper text */
+                messages: {
+                    noResults: '',
+                    results: function () {
+                    }
+                }
+            });
+
         }
         /* compose request and perform it */
         var url = baseUrl + 'customer_accounting/init/';
@@ -52,62 +123,63 @@ define([
         $.ajax(url, opts);
     }
 
-
-    /* functions for accounting slider; TODO: remove me */
-    var fnGetCustomerDataWrap = function () {
-        fnAjaxGetInitData();
+    /**
+     * Search customers on server and prepare data for UI.
+     *
+     * @param request
+     * @param response
+     */
+    var fnAjaxCustomerSearch = function (request, response) {
+        $.ajax({
+            url: urlCustomerSearch,
+            data: {
+                /* see: \Praxigento\Downline\Controller\Adminhtml\Customer\Search::VAR_SEARCH_KEY*/
+                search_key: request.term
+            },
+            success: function (data) {
+                /* convert API data into JQuery widget data */
+                var found = [];
+                for (var i in data.items) {
+                    var one = data.items[i];
+                    var nameFirst = one.name_first;
+                    var nameLast = one.name_last;
+                    var email = one.email;
+                    var id = one.id;
+                    var mlmId = one.mlm_id;
+                    var label = nameFirst + ' ' + nameLast + ' <' + email + '> / ' + mlmId;
+                    var foundOne = {
+                        label: label,
+                        value: label,
+                        data: one
+                    };
+                    found.push(foundOne);
+                }
+                response(found);
+            }
+        });
     }
 
-    var options = {
-        type: 'slide',
-        responsive: true,
-        innerScroll: true,
-        clickableOverlay: true,
-        title: $.mage.__('Accounting'),
-        buttons: [{
-            text: $.mage.__('Process'),
-            class: '',
-            click: function () {
-                this.closeModal();
-            }
-        }, {
-            text: $.mage.__('Get Customer Data'),
-            class: '',
-            click: fnGetCustomerDataWrap
-        }]
-    };
+    var fnAjaxProcessData = function () {
+        var asset = viewModel.selectedAsset();
+        var assetId = asset.asset_id;
+        var amount = viewModel.amount();
+        var customerId = viewModel.customer.id;
+        var counterpartyId = viewModel.selectedCounterparty;
+        var type = viewModel.transferType();
+        if (type == TYPE_BETWEEN) {
 
-    /**
-     * Slider definition.
-     */
-    /* function to load data and display slider */
-    var fnModalOpen = function () {
+        }
+        debugger;
+    }
 
-        /* see ./view/base/web/templates/modal/accounting.html */
-        var modalTmpl = mageTemplate(
-            innerHtml,
-            {
-                formAction: 'changeBalance',
-                assetCode: 'assetCode',
-                balance: 'balalnce',
-                customerName: 'customerName',
-                customerEmail: 'customerEmail',
-                customerRef: 'customerRef',
-                accountId: 'accountId'
-            }
-        );
-        /* set parsed HTML as content for modal placeholder create modal slider */
-        $('#modal_panel_placeholder').html(modalTmpl);
-        var popup = Modal(options, $('#modal_panel_placeholder'));
-
-        /* open modal slider and start ajax request to get init data */
-        popup.openModal();
-        fnAjaxGetInitData();
+    var fnAutocompleteSelected = function (event, ui) {
+        viewModel.selectedCounterparty = ui.item.data.id;
+        debugger;
     }
 
     /* bind modal opening to 'Accounting' button on the form */
     /* (see \Praxigento\Accounting\Block\Customer\Adminhtml\Edit\AccountingButton) */
-    $("#customer-edit-prxgt-accounting").on("click", fnModalOpen);
+    $('#customer-edit-prxgt-accounting').on('click', fnAjaxGetInitData);
 
     /* this is required return to prevent Magento parsing errors */
     var result = Component.extend({});
