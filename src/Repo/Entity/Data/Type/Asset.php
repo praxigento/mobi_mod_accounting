0bdc9d468f61525b9bad942a0c49efad8720fd7b
Alex Gusev <alex@flancer64.com>
2017-11-29 19:01:40 +0200

    MOBI-1005 Add "available" attributes to "prxgt_acc_type_asset"

--- src/Repo/Entity/Data/Type/Asset.php
+++ src/Repo/Entity/Data/Type/Asset.php
@@ -10,5 +10,23 @@ namespace Praxigento\Accounting\Repo\Entity\Data\Type;
 class Asset
     extends \Praxigento\Core\Data\Entity\Type\Base
 {
+    const ATTR_IS_VISIBLE = 'is_visible';
     const ENTITY_NAME = 'prxgt_acc_type_asset';
+
+    /**
+     * @return bool
+     */
+    public function getIsVisible()
+    {
+        $result = parent::get(self::ATTR_IS_VISIBLE);
+        return $result;
+    }
+
+    /**
+     * @param bool $data
+     */
+    public function setIsVisible($data)
+    {
+        parent::set(self::ATTR_IS_VISIBLE, $data);
+    }
 }
