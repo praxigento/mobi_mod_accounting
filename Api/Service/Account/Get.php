8ba6f65b6aaacb0f5c0740b5b565351086514fec
Alex Gusev <alex@flancer64.com>
2018-07-13 15:02:39 +0300

    MOBI-1521 Balance checking for wallet payments

--- Api/Service/Account/Get.php
+++ Api/Service/Account/Get.php
@@ -6,7 +6,8 @@
 namespace Praxigento\Accounting\Api\Service\Account;
 
 /**
- * * TODO: should we use this method or "\Praxigento\Accounting\Repo\Dao\Account::getSystemAccountIdByAssetCode"?
+ * TODO: should we use this method or "\Praxigento\Accounting\Repo\Dao\Account::getSystemAccountIdByAssetCode"?
+ * @deprecated use \Praxigento\Accounting\Repo\Dao\Account
  */
 interface Get
 {
