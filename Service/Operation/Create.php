b6b3d2139d19edca51c5d22786732669e19efa71
Alex Gusev <alex@flancer64.com>
2018-12-19 14:31:03 +0200

    SAN-492 Заказ Erika Seewald ID 790002163 повторился 5 раз, не попал в Odoo

--- Service/Operation/Create.php
+++ Service/Operation/Create.php
@@ -179,14 +179,19 @@ class Create
     private function validateTransactions($trans)
     {
         $result = AResponse::ERR_NO_ERROR;
-        foreach ($trans as $one) {
-            $accDbt = $one->getDebitAccId();
-            $accCrd = $one->getCreditAccId();
-            if ($accDbt == $accCrd) {
-                $result = AResponse::ERR_ONE_ACCOUNT_FOR_DEBIT_AND_CREDIT;
-                break;
+            foreach ($trans as $one) {
+                if (is_array($one)) {
+                    $accDbt = $one[ETrans::A_DEBIT_ACC_ID];
+                    $accCrd = $one[ETrans::A_CREDIT_ACC_ID];
+                } else {
+                    $accDbt = $one->getDebitAccId();
+                    $accCrd = $one->getCreditAccId();
+                }
+                if ($accDbt == $accCrd) {
+                    $result = AResponse::ERR_ONE_ACCOUNT_FOR_DEBIT_AND_CREDIT;
+                    break;
+                }
             }
-        }
         return $result;
     }
 }
