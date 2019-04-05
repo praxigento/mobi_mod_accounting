87ebe278cb3f89387ee6c88a1ee902b4dd7fe68f
Alex Gusev <alex@flancer64.com>
2019-04-05 15:04:15 +0300

    SAN-614 Investigate bonus calcs on test

--- Cli/Balance/Calc.php
+++ Cli/Balance/Calc.php
@@ -15,29 +15,16 @@ class Calc
     const OPT_DAYS_NAME = 'days';
     const OPT_DAYS_SHORTCUT = 'd';
 
-    /** @var \Magento\Framework\DB\Adapter\AdapterInterface */
-    private $conn;
-    /** @var \Psr\Log\LoggerInterface */
-    private $logger;
-    /** @var \Magento\Framework\App\ResourceConnection */
-    private $resource;
     /** @var \Praxigento\Accounting\Api\Service\Account\Balance\Calc */
     private $servBalance;
 
     public function __construct(
-        \Magento\Framework\ObjectManagerInterface $manObj,
-        \Praxigento\Core\Api\App\Logger\Main $logger,
-        \Magento\Framework\App\ResourceConnection $resource,
         \Praxigento\Accounting\Api\Service\Account\Balance\Calc $servBalance
     ) {
         parent::__construct(
-            $manObj,
             'prxgt:acc:balance:calc',
             'Re-calculate accounts balances (reset daily balances up to $days).'
         );
-        $this->logger = $logger;
-        $this->resource = $resource;
-        $this->conn = $resource->getConnection();
         $this->servBalance = $servBalance;
     }
 
@@ -54,30 +41,12 @@ class Calc
 
     }
 
-    protected function execute(
-        \Symfony\Component\Console\Input\InputInterface $input,
-        \Symfony\Component\Console\Output\OutputInterface $output
-    ) {
-        /* get CLI input parameters */
+    protected function process(\Symfony\Component\Console\Input\InputInterface $input)
+    {
         $days = $input->getOption(self::OPT_DAYS_NAME);
-        $msg = "Command '" . $this->getName() . "' (days to reset: $days):";
-        $output->writeln("<info>$msg<info>");
-        $this->logger->info($msg);
-
-        /* wrap all DB operations with DB transaction */
-        $this->conn->beginTransaction();
-        try {
-            $req = new \Praxigento\Accounting\Api\Service\Account\Balance\Calc\Request();
-            $req->setDaysToReset($days);
-            $this->servBalance->exec($req);
-            $this->conn->commit();
-        } catch (\Throwable $e) {
-            $output->writeln('<info>Command \'' . $this->getName() . '\' failed. Reason: '
-                . $e->getMessage() . '<info>');
-            $this->conn->rollBack();
-        }
-        $msg = "Command '" . $this->getName() . "' is completed.";
-        $output->writeln("<info>$msg<info>");
-        $this->logger->info($msg);
+        $this->logInfo("Days to reset: $days.");
+        $req = new \Praxigento\Accounting\Api\Service\Account\Balance\Calc\Request();
+        $req->setDaysToReset($days);
+        $this->servBalance->exec($req);
     }
 }
--- Cli/Balance/Validate.php
+++ Cli/Balance/Validate.php
@@ -15,56 +15,22 @@ use Praxigento\Accounting\Service\Account\Balance\Validate\Response as AResponse
 class Validate
     extends \Praxigento\Core\App\Cli\Cmd\Base
 {
-    /** @var \Magento\Framework\DB\Adapter\AdapterInterface */
-    private $conn;
-    /** @var \Psr\Log\LoggerInterface */
-    private $logger;
-    /** @var \Magento\Framework\App\ResourceConnection */
-    private $resource;
     /** @var \Praxigento\Accounting\Service\Account\Balance\Validate */
     private $servValidate;
 
     public function __construct(
-        \Magento\Framework\ObjectManagerInterface $manObj,
-        \Magento\Framework\App\ResourceConnection $resource,
-        \Praxigento\Core\Api\App\Logger\Main $logger,
-        \Praxigento\Core\Api\App\Repo\Transaction\Manager $manTrans,
         \Praxigento\Accounting\Service\Account\Balance\Validate $servValidate
     ) {
         parent::__construct(
-            $manObj,
             'prxgt:acc:balance:validate',
             'Validate current balances for customers accounts.'
         );
-        $this->logger = $logger;
-        $this->resource = $resource;
-        $this->conn = $resource->getConnection();
         $this->servValidate = $servValidate;
     }
 
-    protected function execute(
-        \Symfony\Component\Console\Input\InputInterface $input,
-        \Symfony\Component\Console\Output\OutputInterface $output
-    ) {
-        /* get CLI input parameters */
-        $msg = "Command '" . $this->getName();
-        $output->writeln("<info>$msg<info>");
-        $this->logger->info($msg);
-
-        /* wrap all DB operations with DB transaction */
-        $this->conn->beginTransaction();
-        try {
-            $req = new ARequest();
-            $this->servValidate->exec($req);
-
-            $this->conn->commit();
-        } catch (\Throwable $e) {
-            $output->writeln('<info>Command \'' . $this->getName() . '\' failed. Reason: '
-                . $e->getMessage() . '.<info>');
-            $this->conn->rollBack();
-        }
-        $msg = "Command '" . $this->getName() . "' is completed.";
-        $output->writeln("<info>$msg<info>");
-        $this->logger->info($msg);
+    protected function process(\Symfony\Component\Console\Input\InputInterface $input)
+    {
+        $req = new ARequest();
+        $this->servValidate->exec($req);
     }
 }
